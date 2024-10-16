745e895f07ab10c46cd7690293708d5c4ef6df81
Amit <Amitshah050112003@gmail.com>
2024-10-16 23:38:12 +0530

    Tailwind setup in React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,11 @@
-import './App.css'
-import {io} from 'socket.io-client'
-import {gql, useQuery} from '@apollo/client'
-
-const Hello_Query = gql`
-query get{
- hello{
-  msg
- }
-}
-`
 
 function App() {
-  const {loading,error,data} = useQuery(Hello_Query);
-  let socket = io("http://localhost:8080");
-
-
-
-  socket.on('connect',()=>{
-    console.log("connected");
-  });
-
-  if(loading) return <p>Loading...</p>
-  // if(error) return <p>{error}</p>
-  if(data){
-    console.log(data);
-  }
   return (
     <>
-    <div>
-      Socket connecting
-    </div>
+     <h1 className="text-3xl  font-bold underline text-red-500">
+      Hello world!
+    </h1>
+
     </>
   )
 }
